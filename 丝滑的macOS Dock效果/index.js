446841161c36e42d7d79a544141d8285ac569427
panpanxiong3 <15160190801@163.com>
2021-05-01 20:03:05 +0800

    提交代码

--- 丝滑的macOS Dock效果/index.js
+++ 丝滑的macOS Dock效果/index.js
@@ -5,7 +5,9 @@ document.querySelectorAll('.dock li').forEach(li => {
 
     li.addEventListener('mousemove', e => {
         let item = e.target
+        // getBoundingClientRect() 方法返回元素的大小及其相对于视口的位置
         let itemRect = item.getBoundingClientRect()
+        // abs() 方法可返回数的绝对值
         let offset = Math.abs(e.clientX - itemRect.left) / itemRect.width
 
         let prev = item.previousElementSibling || null
@@ -16,9 +18,11 @@ document.querySelectorAll('.dock li').forEach(li => {
         resetScale()
 
         if (prev) {
-            prev.style.setProperty('--scale', 1 + scale * Math.abs(offset - 1))
+
+            prev.style.setProperty('--s', 1 + scale * Math.abs(offset - 1))
         }
 
+        //setProperty() 方法用于设置一个新的 CSS 属性，同时也可以修改 CSS 声明块中已存在的属性
         item.style.setProperty('--scale', 1 + scale)
 
         if (next) {
