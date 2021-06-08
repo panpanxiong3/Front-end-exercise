e1e5b32b22396355c4aa07876fa42c8d3bddd15d
panpanxiong3 <15160190801@163.com>
2021-06-08 23:10:48 +0800

    提交代码

--- 照片滑动展示/index.js
+++ 照片滑动展示/index.js
@@ -1,22 +1,23 @@
+// 定义body,css选择body部分
+const body = document.querySelector('body');
 
-/* 定义 body， css 选择 body 部分 */
-const body = document.querySelector("body");
+// 定义lis css选择li这一整体部分
+const lis = document.querySelectorAll("li");
 
-/* 定义 lis， css 选择 li 这一整体部分 */
-const lis = document.querySelectorAll("li")
+window.addEventListener('scroll', () => {
+    onScroll()
+}, false)
 
-/* 事件监听，窗口滑动 */
-window.addEventListener("scroll", () => { onscrl(); }, false);
+onScroll(false);
 
-onscrl(false);
-function onscrl() {
-  const s = (body.scrollHeight - window.innerHeight) / 15;
+function onScroll() {
+    const s = (body.scrollHeight - window.innerHeight) / 15;
 
-  lis.forEach((li, ix) => {
-    const ang = ((this.scrollY - (s * (ix - 1))) * 360 / (s * 2)) - 180;
-    
-    li.style.setProperty('--ang',
-      Math.max(-180, Math.min(180, ang)) + 'deg'
-    );
-  });  
+    lis.forEach((li, index) => {
+        const ang = ((this.scrollY - (s * (index - 1))) * 360 / (s * 2)) - 180;
+
+        li.style.setProperty('--ang',
+            Math.max(-180, Math.min(180, ang)) + 'deg'
+        )
+    })
 }
